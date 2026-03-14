769f87e50588788f3c90a360317a6c5c3c5fea5e
Tawan Sunflower <96534097+natapol2547@users.noreply.github.com>
2026-03-14 23:51:59 +0700

    first commit

---
+++ __init__.py
@@ -0,0 +1,36 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from bpy.utils import register_submodule_factory
+from .custom_icons import load_icons, unload_icons
+
+bl_info = {
+    "name": "Paint System",
+    "author": "Tawan Sunflower, @blastframe",
+    "description": "",
+    "blender": (4, 2, 0),
+    "version": (2, 1, 10),
+    "location": "View3D > Sidebar > Paint System",
+    "warning": "",
+    "category": "Paint",
+    'support': 'COMMUNITY',
+    "tracker_url": "https://github.com/natapol2547/paintsystem"
+}
+
+bl_info_copy = bl_info.copy()
+
+
+def register(): ...
+
+
+def unregister(): ...
